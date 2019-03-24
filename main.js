07fc535997c159701a3b0fd4d4a6cb23f4d326cb
Victor Fortman <Hugovicfortman@outlook.com>
2019-03-24 03:36:23 +0000

    Optimize array usge, less regex, less loops.

--- main.js
+++ main.js
@@ -1,70 +1,70 @@
 
 function main(complexChemicalFormulaString)
 {
-	let chemCompound = sortComplexChemicalToComponents(complexChemicalFormulaString).map(giveElementsMultipliers).reduce(applyParentMultipliers);
-	return getUniqueElementsCount(chemCompound);
+	let y = assignCoefficients(complexChemicalFormulaString);
+		y = applyMultipliers(y);
+		y = getUniqueElementsCount(y);
+
+	console.log(y);
+	return y;
 }
 
-function sortComplexChemicalToComponents(chemicalString)
+// Assign Coefficients to each unique element.
+function assignCoefficients(chemCompound)
 {
-	const simplifiedString = chemicalString.replace(/\)(\d)+/g,'}$1').replace(/\](\d)+/g,'}$1').replace(/\(|\[/g,'{');
-	let rawChemicalArray = [];
-	let processPlant = simplifiedString;
-	const chemicalComplexityDegree = simplifiedString.search(/\{/);
-	for(let x = 0; x <= chemicalComplexityDegree; x++)
-	{
-		if(processPlant.match(/\{[A-Za-z0-9_]*\}/) === null)
-		{
-				continue;
-		}
-		rawChemicalArray.push(processPlant.match(/\{[A-Za-z0-9_]*\}/)[0].replace(/\{|\}/g,''));
-		processPlant = processPlant.replace(/\{[A-Za-z0-9_]*\}/,'_');
-	}
-	rawChemicalArray.push(processPlant);
-	return rawChemicalArray;
-}
-
-
-// The following algo needs refining and refactoring...
-function giveElementsMultipliers(chemCompound)
-{
+	chemCompound = chemCompound.replace(/\)(\d)+/g,'}$1').replace(/\](\d)+/g,'}$1').replace(/\(|\[/g,'{');
 	let numberedCompound = '';
 	for(let x = 0; x < chemCompound.length; x++)
 	{
-		if(chemCompound[x +1] ===undefined)
+		let CanHaveCoefficient = true;
+		numberedCompound += chemCompound[x];
+		CanHaveCoefficient = CanHaveCoefficient & 
+							 isNaN(Number(chemCompound[x])) &
+							 chemCompound[x].match(/\{|\}/) === null;
+		if(chemCompound[x+1] !== undefined)
 		{
-			numberedCompound += (isNaN(Number(chemCompound[x])))? chemCompound[x] + '1' : chemCompound[x];
-			continue;
-		} else if (!isNaN(chemCompound[x])) {
-			numberedCompound += chemCompound[x];
+			CanHaveCoefficient = CanHaveCoefficient & 
+								 isNaN(Number(chemCompound[x + 1])) & 
+								 chemCompound[x + 1].match(/[a-z]/) === null;
+		}
+		if(!CanHaveCoefficient){
 			continue;
 		}
-		if(chemCompound[x +1].match(/[A-Z_]/) !== null && chemCompound[x + 1].match(/[0-9]/) === null)
-		{
-			numberedCompound += chemCompound[x] + '1';
-		}else{
-			numberedCompound += chemCompound[x];
-		}
+		numberedCompound += '1';
 	}
 	return numberedCompound;
 }
-
-function applyMultiplier(chemCompound, multiplier)
+// Serially apply multipliers to nested compounds in reverse order.
+function applyMultipliers(chemCompound)
 {
 	let multipliedCompound = '';
-	for(let x = 0; x < chemCompound.length; x++)
-	{
-		multipliedCompound += (isNaN(Number(chemCompound[x])))? chemCompound[x] : Number(chemCompound[x]) * multiplier;
+	let currentMultiplier = 1, lastCoefficient = 1;
+	let multiplyingFactor = [1];
+	let hitNumberLast = false;
+	for (let x = chemCompound.length - 1; x >= 0; x--) {
+		currentMultiplier = multiplyingFactor.reduce((a,b) => {return a * b;});
+		if (!isNaN(Number(chemCompound[x])))
+		{
+			lastCoefficient = (hitNumberLast)? Number(reverseString(reverseString(lastCoefficient) + chemCompound[x])) : Number(chemCompound[x]);
+			hitNumberLast = true;
+		} else {
+			multipliedCompound += (hitNumberLast)? reverseString(chemCompound[x] + reverseString(lastCoefficient) * currentMultiplier) : chemCompound[x];
+			hitNumberLast = false;
+		}
+		if (chemCompound[x].match(/\}/) !== null){
+			multiplyingFactor.push(lastCoefficient);
+		} else if (chemCompound[x].match(/\{/) !== null){
+			multiplyingFactor.pop();
+		}
 	}
-	return multipliedCompound;
+	return reverseString(multipliedCompound).replace(/\}(\d)+|\{/g,'');
 }
-
-function applyParentMultipliers (a, b)
+// Leverage array functions to reverse strings.
+function reverseString(someString)
 {
-	let multiplier = Number(b.replace(/[A-Za-z0-9]+_(\d)+[A-Za-z0-9]*/,'$1'));
-	return b.replace(/\_(\d)+/,applyMultiplier(a,multiplier));
+	return String(someString).split('').reverse().join('');
 }
-
+// Narrow down and isolate distinct elements with respective occurence.
 function getUniqueElementsCount(compoundString)
 {
 	let compoundArray = compoundString.match(/[A-Za-z]+[0-9]+/g);	
